6e77568872fd52f25033144f8c441a9db742b56c
Alexander Stevenson <27918923+xanderstevenson@users.noreply.github.com>
2022-12-01 22:34:20 -0500

    Update action-text-2-mp3.py

--- action-text-2-mp3.py
+++ action-text-2-mp3.py
@@ -31,12 +31,12 @@ class GetAudio:
         language = "en"
         # load text, convert to mp3, save file and play sample for user
         try:
-            with open(self.text_file) as f:
+            with open(text_file) as f:
                 the_text = f.read()
                 # conversion magic
-                mp3 = gTTS(the_text, lang=language, tld=self.accent)
+                mp3 = gTTS(the_text, lang=language, tld=accent)
                 # strip filename from filepath
-                file_name = ntpath.basename(self.text_file)
+                file_name = ntpath.basename(text_file)
                 # strip file type extension from name
                 file_name = (
                     file_name.replace(".txt", "").replace(".md", "")
@@ -45,14 +45,14 @@ class GetAudio:
                 mp3.save(mp3_base_path + '/mp3s/' + file_name + '.mp3')
                 # Alert use of success and location of mp3
                 click.secho(
-                    f"\n\nMP3 file created at {self.mp3_base_path}/mp3s/{file_name}.mp3\n\n",
+                    f"\n\nMP3 file created at {mp3_base_path}/mp3s/{file_name}.mp3\n\n",
                     fg="green",
                 )
         # handle exception (exits program)
         except FileNotFoundError:
             print(
-                "\n\nERROR\n\nA file named '{}' does not exist. Please try again.\n\n".format(
-                    self.path
+                "\n\nERROR\n\nA file named '{}''{)' does not exist. Please try again.\n\n".format(
+                    mp3_base_path, file_name
                 )
             )
 
