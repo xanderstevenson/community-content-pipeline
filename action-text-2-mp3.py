56042547b078294b75439c1215ec7a860315896e
Alexander Stevenson <27918923+xanderstevenson@users.noreply.github.com>
2022-12-01 22:39:10 -0500

    Update action-text-2-mp3.py

--- action-text-2-mp3.py
+++ action-text-2-mp3.py
@@ -27,39 +27,39 @@ class GetAudio:
         self.accent = accent
 
     # main function
-    def create_mp3(text_file, accent, mp3_base_path):
+    def create_mp3(self):
         language = "en"
         # load text, convert to mp3, save file and play sample for user
         try:
-            with open(text_file) as f:
+            with open(self.text_file) as f:
                 the_text = f.read()
                 # conversion magic
                 mp3 = gTTS(the_text, lang=language, tld=accent)
                 # strip filename from filepath
-                file_name = ntpath.basename(text_file)
+                file_name = ntpath.basename(self.text_file)
                 # strip file type extension from name
                 file_name = (
                     file_name.replace(".txt", "").replace(".md", "")
                 )
                 # save mp3
-                mp3.save(mp3_base_path + '/mp3s/' + file_name + '.mp3')
+                mp3.save(self.mp3_base_path + '/mp3s/' + file_name + '.mp3')
                 # Alert use of success and location of mp3
                 click.secho(
-                    f"\n\nMP3 file created at {mp3_base_path}/mp3s/{file_name}.mp3\n\n",
+                    f"\n\nMP3 file created at {self.mp3_base_path}/mp3s/{file_name}.mp3\n\n",
                     fg="green",
                 )
         # handle exception (exits program)
         except FileNotFoundError:
             print(
-                "\n\nERROR\n\nA file named '{}''{)' does not exist. Please try again.\n\n".format(
-                    mp3_base_path, file_name
+                "\n\nERROR\n\nA file named '{}' does not exist. Please try again.\n\n".format(
+                    file_name
                 )
             )
 
 # Call main class and function
 def cli(text_file, accent, mp3_base_path):
     invoke_class = GetAudio(text_file, accent, mp3_base_path)
-    invoke_class.create_mp3(text_file, accent, mp3_base_path)
+    invoke_class.create_mp3()
 
 
 if __name__ == "__main__":
