e4535358505117c051234f9cfccbc59aafa95cba
Alexander Stevenson <27918923+xanderstevenson@users.noreply.github.com>
2022-12-01 19:59:43 -0500

    Update action-text-2-mp3.py

--- action-text-2-mp3.py
+++ action-text-2-mp3.py
@@ -60,7 +60,7 @@ class GetAudio:
                     file_name.replace(".txt", "").replace(".md", "")
                 )
                 # save mp3
-                mp3.save(f"./mp3s/{file_name}.mp3")
+                mp3.save(f"../mp3s/{file_name}.mp3")
                 # Alert use of success and location of mp3
                 click.secho(
                     f"\n\nMP3 file created at { sys.path[0] }/mp3s/{file_name}.mp3\n\n",
