79d69f67f1873a8752303dfa42848c0fedc2fdb4
Alexander Stevenson <27918923+xanderstevenson@users.noreply.github.com>
2022-12-01 22:28:02 -0500

    Update action-text-2-mp3.py

--- action-text-2-mp3.py
+++ action-text-2-mp3.py
@@ -18,64 +18,36 @@ with contextlib.redirect_stdout(None):
 class GetAudio:
     def __init__(
         self,
-        path,
+        text_file,
         accent,
         mp3_base_path
     ):
-        self.path = path
+        self.text_file = file
         self.mp3_base_path = mp3_base_path
-        # function to assign accent code based on numerical choice
-        def match_accent(accent):
-            match accent:
-                case "1":
-                    return "com.au"
-                case "2":
-                    return "co.uk"
-                case "3":
-                    return "com"
-                case "4":
-                    return "ca"
-                case "5":
-                    return "co.in"
-                case "6":
-                    return "ie"
-                case "7":
-                    return "co.za"
-                case _:
-                    return "com"
-
-        self.accent = match_accent(accent)
+        self.accent = accent
 
     # main function
     def create_mp3(self):
         language = "en"
         # load text, convert to mp3, save file and play sample for user
         try:
-            with open(self.path) as f:
+            with open(self.text_file) as f:
                 the_text = f.read()
                 # conversion magic
                 mp3 = gTTS(the_text, lang=language, tld=self.accent)
                 # strip filename from filepath
-                file_name = ntpath.basename(self.path)
-               
+                file_name = ntpath.basename(self.text_file)
                 # strip file type extension from name
                 file_name = (
                     file_name.replace(".txt", "").replace(".md", "")
                 )
                 # save mp3
-                
                 mp3.save(mp3_base_path + '/mp3s/' + file_name + '.mp3')
                 # Alert use of success and location of mp3
                 click.secho(
                     f"\n\nMP3 file created at {self.mp3_base_path}/mp3s/{file_name}.mp3\n\n",
                     fg="green",
                 )
-#                 # Automatically play audio sample and alert user
-#                 pygame.mixer.init()  # initialize mixer module
-#                 pygame.mixer.music.load(f"{ the_filename }/mp3s/{file_name}.mp3")
-#                 pygame.mixer.music.play()
-#                 print("Audio sample will play for 3 seconds\n\n")
-#                 time.sleep(3)
         # handle exception (exits program)
         except FileNotFoundError:
             print(
@@ -84,42 +56,18 @@ class GetAudio:
                 )
             )
 
-
-# # Prompt user for file input, store as 'path'
-# @click.command()
-# @click.option(
-#     "--path",
-#     prompt="\n\nWELCOME to TEXT-2-MP3\n\nEnter the path of the file to convert text to speech (.txt, .rtf or .md)\n\n",
-#     help="Enter the path of the file to convert text to speech (.txt, .rtf or .md)",
-#     required=True,
-# )
-# # Prompt user for accent input, store as 'accent'
-# @click.option(
-#     "--accent",
-#     prompt="\nPlease choose an English accent (type a number and hit ENTER)\n\n\
-#         1. English (Australia)\n\
-#         2. English (United Kingdom)\n\
-#         3. English (United States)\n\
-#         4. English (Canada)\n\
-#         5. English (India)\n\
-#         6. English (Ireland)\n\
-#         7. English (South Africa)\n\n",
-#     help="Please choose an English accent",
-#     required=True,
-# )
-
-
 # Call main class and function
-def cli(path, accent, mp3_base_path):
+def cli(text_file, accent, mp3_base_path):
     invoke_class = GetAudio(path, accent, mp3_base_path)
     invoke_class.create_mp3()
 
 
 if __name__ == "__main__":
     #import environmental variable from the GitHub actions workflow
-    # strip the brackets and quotes
-    the_filename = "./" + os.getenv('TEST_VAR').replace('[','').replace(']','').replace('"','')
+    # get the file that changed and strip the brackets and quotes
+    the_filename = "./" + (os.getenv('TEST_VAR').replace('[','').replace(']','').replace('"',''))
+    #strip the filename and get the path where the file changed
     mp3_base_path = os.path.dirname(os.path.abspath(the_filename))
-    #run the program with the file and 3 for American accent
-    cli(the_filename, 3, mp3_base_path)
-    print(f"the file name is {the_filename}")
+    #run the program with the file, American accent and path
+    cli(the_filename, "com", mp3_base_path)
+
