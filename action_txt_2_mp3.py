b1d7dc6d90474355bb844503e14c29b97c5fb346
Alexander Stevenson <27918923+xanderstevenson@users.noreply.github.com>
2022-12-10 22:13:47 -0500

    Update action_txt_2_mp3.py

--- action_txt_2_mp3.py
+++ action_txt_2_mp3.py
@@ -21,7 +21,7 @@ def create_mp3(docx_file, accent, mp3_base_path):
     MY_TEXT = docx2txt.process(docx_file)
     # strip filename from filepath
 #     docx_file_name = str(os.path.basename(docx_file).rsplit('.', 1)[0])
-    new_name_text_file = docx_file_name.replace('docx','txt')
+    new_name_text_file = docx_file.replace('docx','txt')
     with open(new_name_text_file, "w") as text_file:
         print(MY_TEXT, file=text_file)
     # load text, convert to mp3, save file and play sample for user
